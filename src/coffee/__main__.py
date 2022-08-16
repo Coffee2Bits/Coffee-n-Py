4b6395721530cc1abe4ba6be5a7e1409357a23c7
Alex Monnet <alexcmonnet@gmail.com>
2022-08-16 20:14:54 +0000

    Update package references

--- src/coffee/__main__.py
+++ src/coffee/__main__.py
@@ -2,13 +2,13 @@
 
 import sys
 
-from example import APP_NAME
+from coffee import APP_NAME
 
 if sys.argv[0].endswith("__main__.py"):
     sys.argv[0] = APP_NAME
 
 
 if __name__ == "__main__":
-    from example.do_something import run
+    from coffee.do_something import run
 
     run()
