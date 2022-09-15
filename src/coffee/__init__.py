25457e2511c7ad3fc22256bd373cda4de24fefb1
Alex Monnet <alexcmonnet@gmail.com>
2022-09-15 02:06:31 +0000

    Update app name

--- src/coffee/__init__.py
+++ src/coffee/__init__.py
@@ -4,7 +4,7 @@ import logging
 import os
 
 __version__ = "0.0.2"
-APP_NAME: str = "coffee"
+APP_NAME: str = "coffee_n_py"
 
 logging.basicConfig(
     format="%(asctime)s::%(module)s::%(filename)s::L%(lineno)d::%(levelname)s::%(message)s",
